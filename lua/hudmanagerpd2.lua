cc412662d83f7be24fae82fd998983468d6fd253
offyerrocker <offyerrocker88@gmail.com>
2025-12-15 23:22:11 -0800

    fixed hide hud callback
    fixed hud not hiding when using the hide-hud keybind from basegame pd2 (wrong hook function)

--- lua/hudmanagerpd2.lua
+++ lua/hudmanagerpd2.lua
@@ -1,10 +1,10 @@
-Hooks:PostHook(HUDManager,"hide_local_player_gear","hudmanager_closedcaptions_hidecaptions",function(self)
+Hooks:PostHook(HUDManager,"set_disabled","hudmanager_closedcaptions_hidecaptions",function(self)
 	if ClosedCaptions.settings.use_hide_hud_keybind then
 		ClosedCaptions:HideCaptionsPanel()
 	end
 end)
 
-Hooks:PostHook(HUDManager,"show_local_player_gear","hudmanager_closedcaptions_showcaptions",function(self)
+Hooks:PostHook(HUDManager,"set_enabled","hudmanager_closedcaptions_showcaptions",function(self)
 	if ClosedCaptions.settings.use_hide_hud_keybind then
 		ClosedCaptions:ShowCaptionsPanel()
 	end
