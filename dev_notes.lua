d93f9cbc2ee8cc006ee9ff1b6ce0cf6db973d41d
offyerrocker <offyerrocker88@gmail.com>
2025-12-15 19:54:51 -0800

    Update dev_notes.lua

--- dev_notes.lua
+++ dev_notes.lua
@@ -3,5 +3,35 @@ TODO
 
 - setup in menu so that captions can be shown in the menu (eg contractor briefings), not just ingame
 
+- better streamlined flow- remove subtitles consistently
+
+
+
+
+
+
+
+
+
+UNUSED LEGACY LOCALIZATIONS:
+	"menu_closedcaptions_set_x_title" : "Set Caption X",
+	"menu_closedcaptions_set_x_desc" : "Change captions' horizontal position",
+	"menu_closedcaptions_set_y_title" : "Set Caption Y",
+	"menu_closedcaptions_set_y_desc" : "Change caption boxes' vertical position",
+	"menu_closedcaptions_set_w_title" : "Set Caption W",
+	"menu_closedcaptions_set_w_desc" : "Change caption boxes' width",
+	"menu_closedcaptions_set_vmargin_title" : "Set Caption Margin",
+	"menu_closedcaptions_set_vmargin_desc" : "Change the vertical gap between caption boxes",
+	"menu_closedcaptions_set_language_title" : "Set Language",
+	"menu_closedcaptions_set_language_desc" : "Change the language of captions' text",
+	"menu_closedcaptions_enable_logging_master_title" : "Master Enable Logging",
+	"menu_closedcaptions_enable_logging_master_desc" : "Must be enabled for any of the below options.",
+	"menu_closedcaptions_enable_logging_missing_title" : "Record Missing IDs",
+	"menu_closedcaptions_enable_logging_missing_desc" : "Writes debug messages to mods/saves/CLOSEDCAPTIONS_MISSING_LINES.txt",
+	"menu_closedcaptions_enable_logging_ids_title" : "Log Incoming Sound IDs",
+	"menu_closedcaptions_enable_logging_ids_desc" : "Outputs sound ids to the BLT log",
+	"menu_closedcaptions_enable_logging_debug_title" : "Log Debug Messages",
+	"menu_closedcaptions_enable_logging_debug_desc" : "Outputs debug messages to the BLT log",
+	"menu_closedcaptions_order_queue" : "Chronological"
 
 --]]
